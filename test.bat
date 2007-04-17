621e63a38d08964fa0cac09cf337bb777c22fb13
yallie_cp <SND\yallie_cp>
2007-04-17 12:08:04 +0400

    Added more test programs (11-Jun-2006 01:40:32)

--- test.bat
+++ test.bat
@@ -1,4 +1,9 @@
 refal.exe test01.ref >tests.log
 refal.exe test02.ref >>tests.log
 refal.exe test03.ref >>tests.log
+refal.exe test04.ref >>tests.log
+refal.exe test05.ref >>tests.log
+refal.exe test06.ref >>tests.log
+refal.exe test07.ref >>tests.log
+refal.exe test08.ref >>tests.log
 start .\tests.log
