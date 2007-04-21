87c5408959e845054551ecdef94bef351eebaec2
yallie_cp <SND\yallie_cp>
2007-04-22 03:41:42 +0400

    Added token recognition; syntax highlighting now basically works

--- vs2005/RefalVsIntegration/RefalLangService/RefalScanner.cs
+++ vs2005/RefalVsIntegration/RefalLangService/RefalScanner.cs
@@ -21,31 +21,148 @@ namespace yallie.RefalLangService
 {
 	class RefalScanner : IScanner
 	{
+		// current line of the source code and scan position
 		string source = null;
 		int offset = 0;
 
+		/// <summary>
+		/// Correspondence table between regex patterns and token colors.
+		/// First entries are matched first.
+		/// </summary>
+		static TokenTableEntry[] tokenTable = new TokenTableEntry[]
+		{
+			// comments
+			new TokenTableEntry(ScanContext.Default, ScanContext.Default, @"\G\/\*.*?\*\/", TokenColor.Comment),
+			new TokenTableEntry(ScanContext.Default, ScanContext.BlockComment, @"\G\/\*.*$", TokenColor.Comment),
+			new TokenTableEntry(ScanContext.BlockComment, ScanContext.Default, @"\G.*?\*\/", TokenColor.Comment),
+			new TokenTableEntry(ScanContext.BlockComment, ScanContext.BlockComment, @".+", TokenColor.Comment),
+			new TokenTableEntry(ScanContext.Default, ScanContext.Default, @"^\s*\*.*$", TokenColor.Comment),
+
+			// whitespace
+			new TokenTableEntry(ScanContext.Default, ScanContext.Default, @"\G\s*", TokenColor.Text),
+
+			// strings
+			new TokenTableEntry(ScanContext.Default, ScanContext.Default, @"\G\""(\\.|[^""\\])*?\""", TokenColor.String),
+			new TokenTableEntry(ScanContext.Default, ScanContext.Default, @"\G\'(\\.|[^'\\])*?\'", TokenColor.String),
+
+			// free variables
+			new TokenTableEntry(ScanContext.Default, ScanContext.Default, @"\G[ets]\.", TokenColor.Keyword),
+
+			// keywords
+			new TokenTableEntry(ScanContext.Default, ScanContext.Default, @"\G\$((ENTRY)|(EXTRN)|(EXTERN)|(EXTERNAL))", TokenColor.Keyword),
+			new TokenTableEntry(ScanContext.Default, ScanContext.Default, @"\G((True)|(False))", TokenColor.Keyword),
+
+			// numbers
+			new TokenTableEntry(ScanContext.Default, ScanContext.Default, @"\G[\d]+(([Uu][Ll]?)|([Ll][Uu]?))?", TokenColor.Text),
+			new TokenTableEntry(ScanContext.Default, ScanContext.Default, @"\G0[xX][\dA-Fa-f]+(([Uu][Ll]?)|([Ll][Uu]?))", TokenColor.Text),
+
+			// identifiers
+			new TokenTableEntry(ScanContext.Default, ScanContext.Default, @"\G[A-Za-z][A-Za-z\d_-]*", TokenColor.Text),
+
+			// others
+			new TokenTableEntry(ScanContext.Default, ScanContext.Default, @".", TokenColor.Text)
+		};
+
 		public bool ScanTokenAndProvideInfoAboutIt(TokenInfo tokenInfo, ref int state)
 		{
-			if (source == null || source.Length == 0)
+			// is it the end?
+			if (source == null || offset >= source.Length)
 				return false;
 
-			if (tokenInfo != null)
+			// find the match within the current scan context (text, comment, string, etc.)
+			foreach (TokenTableEntry t in tokenTable)
 			{
-				tokenInfo.Type = TokenType.Comment;
-				tokenInfo.Color = TokenColor.Comment;
-				tokenInfo.StartIndex = offset;
-				tokenInfo.EndIndex = ++offset;
+				if (t.InputContext != (ScanContext)state)
+					continue;
+
+				Match match = t.Regex.Match(source, offset);
+				if (match.Success && match.Length > 0)
+				{
+					// set ouput context and token information
+					state = (int)t.OutputContext;
+					if (tokenInfo != null)
+					{
+						tokenInfo.Color = t.TokenColor;
+						tokenInfo.Type = TokenType.Text;
+						tokenInfo.StartIndex = offset;
+						tokenInfo.EndIndex = offset + match.Length - 1;
+					}
+
+					// update offset
+					offset += match.Length;
+					return true;
+				}
 			}
 
-			source = source.Substring(1);
+			// no matches => skip one character
+			if (tokenInfo != null)
+			{
+				tokenInfo.Color = TokenColor.Text;
+				tokenInfo.Type = TokenType.Text;
+				tokenInfo.StartIndex = offset;
+				tokenInfo.EndIndex = offset + 1;
+			}
+
+			// increase current offset
+			offset++;
 			return true;
 		}
 
 		public void SetSource(string source, int offset)
 		{
-			System.Windows.Forms.MessageBox.Show("Offset = " + offset + ", text:\r\n\r\n" + source);
 			this.source = source;
 			this.offset = offset;
+		}
+	}
+
+	/// <summary>
+	/// Visual Studio scans input file line-by-line, so scanner can stop
+	/// inside block comment, or character string.
+	/// </summary>
+	internal enum ScanContext
+	{
+		Default,
+		BlockComment,
+		String,
+		Character,
+		Error
+	}
+
+	/// <summary>
+	/// This class stores information about patterns and token colors.
+	/// </summary>
+	internal class TokenTableEntry
+	{
+		readonly ScanContext inputContext, outputContext;
+		readonly Regex regExpression;
+		readonly TokenColor tokenColor;
+
+		public TokenTableEntry(ScanContext inputContext, ScanContext outputContext, string pattern, TokenColor color)
+		{
+			this.inputContext = inputContext;
+			this.outputContext = outputContext;
+			this.regExpression = new Regex(/*"\\G" + */ pattern); // \G = "anchor to the current position"
+			this.tokenColor = color;
+		}
+
+		public ScanContext InputContext
+		{
+			get { return inputContext; }
+		}
+
+		public ScanContext OutputContext
+		{
+			get { return outputContext; }
+		}
+
+		public Regex Regex
+		{
+			get { return regExpression; }
+		}
+
+		public TokenColor TokenColor
+		{
+			get { return tokenColor; }
 		}
 	}
 }
